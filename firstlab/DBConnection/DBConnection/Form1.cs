c1db2e3c120555d33dd627de9d77b7bdcdc551f9
alexamikaya <alexamikaya@gmail.com>
2021-07-22 22:33:00 +0300

    Add project files.

---
+++ firstlab/DBConnection/DBConnection/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DBConnection
+{
+    public partial class DBConnection : Form
+    {
+        OleDbConnection connection = new OleDbConnection();
+        //string testConnect = @"Provider=SQLOLEDB.1;Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=AdventureWorks2017;Data Source=WIN-U083NMJAPKD\SQLEXPRESS";
+
+        public DBConnection()
+        {
+            InitializeComponent();
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void DBConnection_Load(object sender, EventArgs e)
+        {
+            this.connection.StateChange += new
+                System.Data.StateChangeEventHandler(
+                this.connection_StateChange);
+
+        }
+        static string GetConnectionStringByName(string name)
+        {
+            string returnValue = null;
+            ConnectionStringSettings settings =
+                ConfigurationManager.ConnectionStrings[name];
+            if (settings != null)
+                returnValue = settings.ConnectionString;
+            return returnValue;
+        }
+        string testConnect = GetConnectionStringByName("DBConnect.NorthwindConnectionString");
+        private void connection_StateChange(object sender, System.Data.StateChangeEventArgs e)
+        {
+            connectionToolStripMenuItem.Enabled =
+                (e.CurrentState == ConnectionState.Closed);
+            cancelToolStripMenuItem.Enabled =
+                (e.CurrentState == ConnectionState.Open);
+        }
+
+        private void connectionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                {
+                    connection.ConnectionString = testConnect;
+                    connection.Open();
+                    MessageBox.Show("Соединение с базой данных выполнено успешно");
+                }
+                else
+                    MessageBox.Show("Соединение с базой данных уже установлено");
+            }
+            catch (Exception Xcp)
+            {
+                MessageBox.Show(Xcp.Message, "Unexpected Exception",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+                MessageBox.Show("Соединение с базой данных закрыто");
+            }
+            else
+                MessageBox.Show("Соединение с базой данных уже закрыто");
+
+        }
+
+        private void cancelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void connectionListToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ConnectionStringSettingsCollection settings = ConfigurationManager.ConnectionStrings;
+            if (settings != null)
+            {
+                foreach (ConnectionStringSettings cs in settings)
+                {
+                    MessageBox.Show("name = " + cs.Name);
+                    MessageBox.Show("providerName = " + cs.ProviderName);
+                    MessageBox.Show("connectionString = " + cs.ConnectionString);
+                }
+            }
+
+
+        }
+    }
+}
